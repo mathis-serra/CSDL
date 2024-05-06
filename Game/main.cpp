1705c0728e9951534c3b278a2bd9deb15c3fd49f
mathis-serra <mathis.serra@laplateforme.io>
2024-05-06 11:18:47 +0200

    triyng to make sfml work

--- Game/main.cpp
+++ Game/main.cpp
@@ -1,7 +1,19 @@
-#include<iostream>
 #include <SFML/Graphics.hpp>
-using namespace std;
 
-int main()
-{
+int main() {
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML Window");
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
+
+        window.clear(sf::Color::White);
+        window.display();
+    }
+
+    return 0;
 }
